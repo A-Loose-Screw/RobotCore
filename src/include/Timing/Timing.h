7301dd7fb88de4dcd81d1cd8e0c8a12752f7d4e2
CJBuchel <connorbuchel@gmail.com>
2022-12-17 05:42:01 +0800

    Removed _V2 chrono for linux

--- src/include/Timing/Timing.h
+++ src/include/Timing/Timing.h
@@ -33,13 +33,7 @@ namespace RBC {
        * @brief high resolution time_stamp
        * 
        */
-      #ifdef RBC_MSC
       typedef std::chrono::high_resolution_clock::time_point time_stamp;
-      #elif defined(RBC_GCC) || defined(RBC_CLANG)
-      typedef std::chrono::_V2::high_resolution_clock::time_point time_stamp;
-      #else
-      #error "Compiler not supported"
-      #endif
 
       /**
        * @brief Get the System Timestamp
